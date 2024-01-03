4c09f911a6605c855e482eb85affb31d2cb8e6b0
Eduard Chyzhyk <e.chyzhyk@mageworx.com>
2024-01-03 22:47:04 +0100

    add simple web app

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta name="description" content="=description=" />
+  <meta charset="utf-8">
+  <title>Kubernetes lab</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="Eduard Chyzhik">
+</head>
+
+<body>
+  
+<p>
+    Eduard Chyzhik
+</p>
+<p>
+    version: 1.0.0
+</p>
+
+</body>
+</html>
